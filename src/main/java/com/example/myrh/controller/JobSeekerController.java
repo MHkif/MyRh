df867ab64b269f070e34c9892db76d547b44c823
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-01-07 16:54:04 +0100

    ☕️️ [PDR-44][FEATURE ][JOB-SEEKER][ Avoir tous les candidats filtré par type de candidature (Online ou Offline) ou par titre de l'offre ]

--- src/main/java/com/example/myrh/controller/JobSeekerController.java
+++ src/main/java/com/example/myrh/controller/JobSeekerController.java
@@ -4,21 +4,26 @@ package com.example.myrh.controller;
 import com.example.myrh.dto.requests.JobSeekerReq;
 import com.example.myrh.dto.responses.JobSeekerRes;
 
+import com.example.myrh.service.IJobSeekerFilterService;
 import com.example.myrh.service.IJobSeekerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("myrh/api/v1/jobSeekers")
 @CrossOrigin("*")
 public class JobSeekerController {
     private final IJobSeekerService service;
+    private final IJobSeekerFilterService jobSeekerFilterService;
 
     @Autowired
-    public JobSeekerController(IJobSeekerService service) {
+    public JobSeekerController(IJobSeekerService service, IJobSeekerFilterService jobSeekerFilterService) {
         this.service = service;
+        this.jobSeekerFilterService = jobSeekerFilterService;
     }
 
     @PostMapping("")
@@ -28,12 +33,24 @@ public class JobSeekerController {
     }
 
     @GetMapping("")
-    public ResponseEntity<Page<JobSeekerRes>> getAll(@RequestParam int page, @RequestParam int size) {
+    public ResponseEntity<Page<JobSeekerRes>> getAll(@RequestParam int page, @RequestParam int size ) {
         return ResponseEntity.ok(service.getAll(page, size));
+    }
+    // TODO : 7-01-2024 Avoir tous les candidats filtré par type de candidature (Online ou Offline) ou par titre de l'offre
+    @GetMapping("/filter")
+    public ResponseEntity<Page<JobSeekerRes>> filterAll(
+            @RequestParam  Map<String,String> params) {
+        return ResponseEntity.ok(jobSeekerFilterService.filterAll(params));
     }
 
     @GetMapping("{id}")
     public ResponseEntity<JobSeekerRes> get(@PathVariable int id) {
         return ResponseEntity.ok(service.getById(id));
+        
     }
+    
+
+
+    
+    
 }
--- src/main/java/com/example/myrh/repository/JobSeekerRepo.java
+++ src/main/java/com/example/myrh/repository/JobSeekerRepo.java
@@ -1,8 +1,14 @@
 package com.example.myrh.repository;
 
+import com.example.myrh.enums.UserStatus;
 import com.example.myrh.model.JobSeeker;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
 
 public interface JobSeekerRepo extends JpaRepository<JobSeeker, Integer> {
     boolean existsByEmail(String email);
+    Page<JobSeeker> getAllByStatus(UserStatus userStatus, Pageable pageable);
 }
