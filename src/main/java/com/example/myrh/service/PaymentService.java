4911c1a577f7ac8f50784f96b7e2c8bbb77a5c6c
e.elhjoujy <e.elhjoujy@labelvie.ma>
2024-01-12 20:44:13 +0100

    [INCLUDE STRIPE IMPLEMENTING INCLUDING FAKE StripeHistory TO MANAGE THE PAYMENT PROCESS ]

--- src/main/java/com/example/myrh/service/PaymentService.java
+++ src/main/java/com/example/myrh/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.example.myrh.service;
 
+import com.example.myrh.dto.StripeChargeDto;
+import com.example.myrh.dto.StripeTokenDto;
 import com.stripe.exception.StripeException;
 
 public interface PaymentService {
@@ -9,5 +11,7 @@ public interface PaymentService {
     //based on the subscription status, the company will be able to pay for the subscription
     boolean pay(String token , double amount)throws StripeException;
     boolean cancel(String companyId);
+    StripeTokenDto createCardToken(StripeTokenDto model);
+    StripeChargeDto charge(StripeChargeDto stripeChargeDto);
 
 }
